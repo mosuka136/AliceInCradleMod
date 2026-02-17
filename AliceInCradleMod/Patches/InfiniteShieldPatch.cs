d9435b2c619b019470b12ff07613e5175c92d536
febai <82594011+febai@users.noreply.github.com>
2026-02-17 10:35:19 +0800

    feat(patch): 增加了无限护盾的功能

---
+++ AliceInCradleMod/Patches/InfiniteShieldPatch.cs
@@ -0,0 +1,23 @@
+using HarmonyLib;
+using nel;
+
+namespace BetterExperience.Patches
+{
+    internal partial class Patchs
+    {
+        [HarmonyPatch]
+        private class InfiniteShieldPatch
+        {
+            [HarmonyPatch(typeof(M2Shield), "canGuard")]
+            private static bool Prefix(M2Shield __instance, ref bool __result)
+            {
+                if (!ConfigManager.EnableInfiniteShield.Value)
+                    return true;
+
+                __instance.cure();
+
+                return true;
+            }
+        }
+    }
+}
