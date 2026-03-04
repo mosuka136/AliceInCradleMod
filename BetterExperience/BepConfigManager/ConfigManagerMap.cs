c1a36003bd8575cca72be2a11d497388401ee181
febai <82594011+febai@users.noreply.github.com>
2026-03-04 13:25:23 +0800

    feat(patch): 增加设置危险度的功能

--- BetterExperience/BepConfigManager/ConfigManagerMap.cs
+++ BetterExperience/BepConfigManager/ConfigManagerMap.cs
@@ -11,6 +11,8 @@ namespace BetterExperience.BepConfigManager
         public static ConfigEntry<bool> EnableMapDamage { get; private set; }
         public static ConfigEntry<bool> EnableDrowning { get; private set; }
         public static ConfigEntry<bool> EnableDarkArea { get; private set; }
+        public static ConfigEntry<bool> EnablePreloadDangerLevel { get; private set; }
+        public static ConfigEntry<int> SetDangerLevel { get; private set; }
 
         private const string SectionMap = "Map";
 
@@ -63,6 +65,21 @@ namespace BetterExperience.BepConfigManager
                 true,
                 "Enable dark area. After disabling, specific areas will no longer require the Magic Bug Lantern to illuminate.\n" +
                 "启用黑暗区域。禁用后特定区域将不再需要魔荧虫提灯照亮。"
+                );
+            EnablePreloadDangerLevel = Config.Bind(
+                SectionMap,
+                nameof(EnablePreloadDangerLevel),
+                false,
+                "Enable preload danger level. When enabled, the danger level will be automatically set once after loading a save, " +
+                "using the configured value to override the original danger level.\n" +
+                "启用预加载危险度。开启后，危险度将在存档读取后自动设置一次，使用设置值覆盖原始危险度。"
+                );
+            SetDangerLevel = Config.Bind(
+                SectionMap,
+                nameof(SetDangerLevel),
+                -1,
+                "Set the danger level. It will override the original danger level. Set to -1 to keep the danger level at its current value.\n" +
+                "设置危险度。将覆盖原始的危险度。设为 -1 可保持为当前值。"
                 );
         }
     }
---
+++ BetterExperience/Patches/SetDangerLevelPatch.cs
@@ -0,0 +1,53 @@
+using BetterExperience.BepConfigManager;
+using HarmonyLib;
+using nel;
+
+namespace BetterExperience.Patches
+{
+    internal partial class HPatches
+    {
+        [HarmonyPatch]
+        private class SetDangerLevelPatch
+        {
+            private static bool _initialized = false;
+
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(FrameUpdateBooster), nameof(FrameUpdateBooster.Awake))]
+            private static void Initialize()
+            {
+                if (_initialized)
+                    return;
+
+                GameAttributePatchManager.Instance.OnGameSaveLoadCompleted += () =>
+                {
+                    if (ConfigManager.EnablePreloadDangerLevel.Value)
+                        SetDangerLevel(ConfigManager.SetDangerLevel.Value);
+                };
+
+                ConfigManager.SetDangerLevel.SettingChanged += (s, e) =>
+                {
+                    SetDangerLevel(ConfigManager.SetDangerLevel.Value);
+                };
+
+                _initialized = true;
+            }
+
+            public static void SetDangerLevel(int level)
+            {
+                if (level < 0)
+                    return;
+
+                var sg = UnityEngine.Object.FindAnyObjectByType<SceneGame>();
+                if (sg == null)
+                    return;
+
+                var m2d = Traverse.Create(sg).Field("M2D").GetValue<NelM2DBase>();
+                if (m2d == null)
+                    return;
+
+                Traverse.Create(m2d.NightCon).Field("dlevel").SetValue(level);
+                m2d.NightCon.showNightLevelAdditionUI(true);
+            }
+        }
+    }
+}
