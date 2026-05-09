68c535d0efebb807d9c7b3da946c9e12c1ecb312
febai <82594011+febai@users.noreply.github.com>
2026-05-09 23:05:04 +0800

    fix(patch): 修复了在拥有异常状态时启用免疫异常状态后加载存档时无法加载存档的问题

--- BetterExperience/Patches/ImmuneAbnormalitiesPatch.cs
+++ BetterExperience/Patches/ImmuneAbnormalitiesPatch.cs
@@ -2,6 +2,8 @@ using BetterExperience.BConfigManager;
 using HarmonyLib;
 using m2d;
 using nel;
+using System;
+using System.Collections.Generic;
 
 namespace BetterExperience.Patches
 {
@@ -10,75 +12,71 @@ namespace BetterExperience.Patches
         [HarmonyPatch]
         public class ImmuneAbnormalitiesPatch
         {
+            private static bool _isLoading = false;
+
+            private static readonly IDictionary<SER, Func<bool>> ImmuneAbnormalityConfigMap = new Dictionary<SER, Func<bool>>
+            {
+                { SER.MP_REDUCE, () => ConfigManager.EnableImmuneAbnormalityMpReduce.Value },
+                { SER.BURST_TIRED, () => ConfigManager.EnableImmuneAbnormalityBurstTired.Value },
+                { SER.CLT_BROKEN, () => ConfigManager.EnableImmuneAbnormalityClothesBroken.Value },
+                { SER.OVERRUN_TIRED, () => ConfigManager.EnableImmuneAbnormalityOverRunTired.Value },
+                { SER.SHIELD_BREAK, () => ConfigManager.EnableImmuneAbnormalityShieldBreak.Value },
+                { SER.SLEEP, () => ConfigManager.EnableImmuneAbnormalitySleep.Value },
+                { SER.BURNED, () => ConfigManager.EnableImmuneAbnormalityBurned.Value },
+                { SER.FROZEN, () => ConfigManager.EnableImmuneAbnormalityFrozen.Value },
+                { SER.PARALYSIS, () => ConfigManager.EnableImmuneAbnormalityParalysis.Value },
+                { SER.CONFUSE, () => ConfigManager.EnableImmuneAbnormalityConfuse.Value },
+                { SER.JAMMING, () => ConfigManager.EnableImmuneAbnormalityJamming.Value },
+                { SER.PARASITISED, () => ConfigManager.EnableImmuneAbnormalityParasitised.Value },
+                { SER.SHAMED, () => ConfigManager.EnableImmuneAbnormalityShamed.Value },
+                { SER.SHAMED_SPLIT, () => ConfigManager.EnableImmuneAbnormalityShamedSplit.Value },
+                { SER.SHAMED_WET, () => ConfigManager.EnableImmuneAbnormalityShamedWet.Value },
+                { SER.SHAMED_EP, () => ConfigManager.EnableImmuneAbnormalityShamedEp.Value },
+                { SER.SEXERCISE, () => ConfigManager.EnableImmuneAbnormalitySexercise.Value },
+                { SER.FRUSTRATED, () => ConfigManager.EnableImmuneAbnormalityFrustrated.Value },
+                { SER.ORGASM_AFTER, () => ConfigManager.EnableImmuneAbnormalityOrgasmAfter.Value },
+                { SER.EGGED, () => ConfigManager.EnableImmuneAbnormalityEgged.Value },
+                { SER.LAYING_EGG, () => ConfigManager.EnableImmuneAbnormalityLayingEgg.Value },
+                { SER.DO_NOT_LAY_EGG, () => ConfigManager.EnableImmuneAbnormalityDoNotLayEgg.Value },
+                { SER.NEAR_PEE, () => ConfigManager.EnableImmuneAbnormalityNearPee.Value },
+                { SER.DRUNK, () => ConfigManager.EnableImmuneAbnormalityDrunk.Value },
+                { SER.WEB_TRAPPED, () => ConfigManager.EnableImmuneAbnormalityWebTrapped.Value },
+                { SER.STONE, () => ConfigManager.EnableImmuneAbnormalityStone.Value },
+                { SER.ATK_DOWN, () => ConfigManager.EnableImmuneAbnormalityAtkDown.Value }
+            };
+
             [HarmonyPrefix]
             [HarmonyPatch(typeof(M2Ser), nameof(M2Ser.Add))]
-            public static bool Prefix(SER ser)
+            public static bool AddPrefix(M2Ser __instance, SER ser)
             {
-                if (ConfigManager.EnableImmuneAbnormalities.Value)
+                try
                 {
-                    HLog.Debug($"{nameof(ImmuneAbnormalitiesPatch)} applied.");
-                    return false;
+                    if (!_isLoading && (ConfigManager.EnableImmuneAbnormalities.Value || (ImmuneAbnormalityConfigMap.TryGetValue(ser, out var isEnabled) && isEnabled())))
+                    {
+                        HLog.Debug($"{nameof(ImmuneAbnormalitiesPatch)} applied for {ser}.");
+                        return false;
+                    }
+                    return true;
                 }
+                catch (Exception ex)
+                {
+                    HLog.Error($"Unexpected error in {nameof(ImmuneAbnormalitiesPatch)}", ex);
+                    return true;
+                }
+            }
 
-                switch (ser)
-                {
-                    case SER.MP_REDUCE:
-                        return !ConfigManager.EnableImmuneAbnormalityMpReduce.Value;
-                    case SER.BURST_TIRED:
-                        return !ConfigManager.EnableImmuneAbnormalityBurstTired.Value;
-                    case SER.CLT_BROKEN:
-                        return !ConfigManager.EnableImmuneAbnormalityClothesBroken.Value;
-                    case SER.OVERRUN_TIRED:
-                        return !ConfigManager.EnableImmuneAbnormalityOverRunTired.Value;
-                    case SER.SHIELD_BREAK:
-                        return !ConfigManager.EnableImmuneAbnormalityShieldBreak.Value;
-                    case SER.SLEEP:
-                        return !ConfigManager.EnableImmuneAbnormalitySleep.Value;
-                    case SER.BURNED:
-                        return !ConfigManager.EnableImmuneAbnormalityBurned.Value;
-                    case SER.FROZEN:
-                        return !ConfigManager.EnableImmuneAbnormalityFrozen.Value;
-                    case SER.PARALYSIS:
-                        return !ConfigManager.EnableImmuneAbnormalityParalysis.Value;
-                    case SER.CONFUSE:
-                        return !ConfigManager.EnableImmuneAbnormalityConfuse.Value;
-                    case SER.JAMMING:
-                        return !ConfigManager.EnableImmuneAbnormalityJamming.Value;
-                    case SER.PARASITISED:
-                        return !ConfigManager.EnableImmuneAbnormalityParasitised.Value;
-                    case SER.SHAMED:
-                        return !ConfigManager.EnableImmuneAbnormalityShamed.Value;
-                    case SER.SHAMED_SPLIT:
-                        return !ConfigManager.EnableImmuneAbnormalityShamedSplit.Value;
-                    case SER.SHAMED_WET:
-                        return !ConfigManager.EnableImmuneAbnormalityShamedWet.Value;
-                    case SER.SHAMED_EP:
-                        return !ConfigManager.EnableImmuneAbnormalityShamedEp.Value;
-                    case SER.SEXERCISE:
-                        return !ConfigManager.EnableImmuneAbnormalitySexercise.Value;
-                    case SER.FRUSTRATED:
-                        return !ConfigManager.EnableImmuneAbnormalityFrustrated.Value;
-                    case SER.ORGASM_AFTER:
-                        return !ConfigManager.EnableImmuneAbnormalityOrgasmAfter.Value;
-                    case SER.EGGED:
-                        return !ConfigManager.EnableImmuneAbnormalityEgged.Value;
-                    case SER.LAYING_EGG:
-                        return !ConfigManager.EnableImmuneAbnormalityLayingEgg.Value;
-                    case SER.DO_NOT_LAY_EGG:
-                        return !ConfigManager.EnableImmuneAbnormalityDoNotLayEgg.Value;
-                    case SER.NEAR_PEE:
-                        return !ConfigManager.EnableImmuneAbnormalityNearPee.Value;
-                    case SER.DRUNK:
-                        return !ConfigManager.EnableImmuneAbnormalityDrunk.Value;
-                    case SER.WEB_TRAPPED:
-                        return !ConfigManager.EnableImmuneAbnormalityWebTrapped.Value;
-                    case SER.STONE:
-                        return !ConfigManager.EnableImmuneAbnormalityStone.Value;
-                    case SER.ATK_DOWN:
-                        return !ConfigManager.EnableImmuneAbnormalityAtkDown.Value;
-                    default:
-                        return true;
-                }
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(M2Ser), nameof(M2Ser.readBinaryFrom))]
+            public static void ReadBinaryFromPrefix()
+            {
+                _isLoading = true;
+            }
+
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(M2Ser), nameof(M2Ser.readBinaryFrom))]
+            public static void ReadBinaryFromPostfix()
+            {
+                _isLoading = false;
             }
         }
     }
